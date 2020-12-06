35d1c1980169777218499050d6d56dfc1ac3af17
Maciej Dybowski <maciej.dybowski@tecna.pl>
2020-12-06 19:11:28 +0100

    initial commit

---
+++ src/main/java/pl/jenkins/demo/jenkinsdemoapp/JenkinsDemoAppApplication.java
@@ -0,0 +1,21 @@
+package pl.jenkins.demo.jenkinsdemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsDemoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsDemoAppApplication.class, args);
+    }
+
+
+    @GetMapping
+    public String get(){
+        return "Łap wiadomosc";
+    }
+}
